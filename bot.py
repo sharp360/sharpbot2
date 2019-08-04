6379bc3735e09febc8e32996a4b88116dcb87431
SharpLines <savapoopurka2@gmail.com>
2019-08-04 12:20:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ async def coinflip(ctx):
 def check_if_it_is_me(ctx):
     return ctx.message.author.id == 203634439673217024
 
-@bot.command()
+@client.command()
 @commands.check(check_if_it_is_me)
 async def only_for_me(ctx):
     await ctx.send('Я знаю тебя!') 
