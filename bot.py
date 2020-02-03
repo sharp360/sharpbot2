ddfdbd2d24b5ad7c6f5e6f6a4838e5b5d2ae797a
Savelii Frolov <sharplineses@gmail.com>
2020-02-03 19:43:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,19 +29,6 @@ async def on_ready():
     print('Ебать работает')
 
 @bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
-
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-
-@bot.command()
 async def help(ctx):
     await ctx.send("Доступные команды: /clear, /зачистка, /kick, /ban, /unban, /dice, /megadice, /coinflip, /invite, /join, /leave, /play, /pause, /stop")
 
