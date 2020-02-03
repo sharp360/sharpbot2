3dd4510092312eea4ac9ed34f53794d88485d6cb
Savelii Frolov <sharplineses@gmail.com>
2020-02-03 22:17:28 +0200

    Update musicbot.py

--- cogs/musicbot.py
+++ cogs/musicbot.py
@@ -82,7 +82,7 @@ class Music(commands.Cog):
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-        await ctx.send('Now playing: {}'.format(player.title))
+        await ctx.send('Проигрываю: {}'.format(player.title))
 
     @commands.command(aliases=['v'])
     @commands.is_owner()
