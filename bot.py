7ad5af0048c2aa8b7f95009ea0b5610a43de3882
Savelii Frolov <sharplineses@gmail.com>
2020-05-02 00:38:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def join(ctx):
     
     await ctx.send(f"Бот присоединился в {channel}")
 
-@bot.command(pass_context=True, aliases=['stop, p'])
+@bot.command(pass_context=True)
 async def pause(ctx):
 
     voice = get(bot.voice_clients, guild=ctx.guild)
