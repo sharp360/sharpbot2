7ce3f3c650b8e2ecb0679ec9fee5384b0f0e615d
SharpLines <31456845+SharpLines@users.noreply.github.com>
2020-07-19 09:43:07 +0300

    Create helper.py

---
+++ cogs/utils/helper.py
@@ -0,0 +1,53 @@
+import asyncio
+import discord
+import logging
+import random
+
+from .gets import getColor
+
+log = logging.getLogger('LOG')
+
+
+# Edit thingy
+async def edit(ctx, content=None, embed=None, ttl=None):
+    perms = ctx.channel.permissions_for(ctx.me).embed_links
+    ttl = None if ctx.message.content.endswith(' stay') else ttl
+    try:
+        if ttl and perms:
+            await ctx.message.edit(content=content, embed=embed)
+            await asyncio.sleep(ttl)
+            try:
+                await ctx.message.delete()
+            except:
+                log.error('Failed to delete Message in {}, #{}'.format(ctx.guild.name, ctx.channel.name))
+                pass
+        elif ttl is None and perms:
+            await ctx.message.edit(content=content, embed=embed)
+        elif embed is None:
+            await ctx.message.edit(content=content, embed=embed)
+        elif embed and not perms:
+            await ctx.message.edit(content='\N{HEAVY EXCLAMATION MARK SYMBOL} No Perms for Embeds', delete_after=5)
+    except:
+        if embed and not perms:
+            await ctx.message.edit(content='\N{HEAVY EXCLAMATION MARK SYMBOL} No Perms for Embeds', delete_after=5)
+        else:
+            await ctx.send(content=content, embed=embed, delete_after=ttl, file=None)
+
+
+# Check for perms of links and attached files
+def permFile(message):
+    return message.channel.permissions_for(message.author).attach_files
+
+
+# Check for perms of links and attached files
+def permEmbed(message):
+    return message.channel.permissions_for(message.author).embed_links
+
+
+# Check for perms of links and attached files
+def embedColor(self):
+    if self.bot.embed_color == "" or getColor(self.bot.embed_color) is None:
+        color = discord.Color(random.randint(0, 0xFFFFFF))
+    else:
+        color = int((getColor(self.bot.embed_color).hex_l.strip('#')), 16)
+    return color
